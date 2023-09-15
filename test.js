1af1c5601c776439a9ded78186c82d8ebc34c575
Yuanlailai <46029173+Yuanlailai@users.noreply.github.com>
2023-09-15 17:00:05 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,54 @@
+{
+    "default": {
+        "navigation": [
+            {
+                "id": "briar-embed-2023",
+                "displayName": "贝蕾亚",
+                "isPlugin": false,
+                "enabled": true,
+                "visible": true,
+                "url": "https://embed.rgpub.io/wwpub-briar-embed-2023/zh-cn/",
+                "priority": 10
+            },
+            {
+                "id": "overview",
+                "displayName": "总览",
+                "isPlugin": false,
+                "enabled": true,
+                "visible": true,
+                "url": "https://static.rgpub.io/pcs/leagueclient/home-overview/",
+                "priority": 20
+            }
+        ]
+    },
+    "npe": {
+        "navigation": [
+            {
+                "id": "rcp-fe-lol-npe-rewards",
+                "displayName": "旅程",
+                "isPlugin": true,
+                "enabled": true,
+                "visible": true,
+                "priority": 10
+            },
+            {
+                "id": "briar-embed-2023",
+                "displayName": "贝蕾亚",
+                "isPlugin": false,
+                "enabled": true,
+                "visible": true,
+                "url": "https://embed.rgpub.io/wwpub-briar-embed-2023/zh-cn/",
+                "priority": 20
+            },
+            {
+                "id": "overview",
+                "displayName": "总览",
+                "isPlugin": false,
+                "enabled": true,
+                "visible": true,
+                "url": "https://static.rgpub.io/pcs/leagueclient/home-overview/",
+                "priority": 30
+            }
+        ]
+    }
+}
